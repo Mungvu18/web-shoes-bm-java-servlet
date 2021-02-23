1b4e8ea942697aab187f270bdb47e56bcc52c416
MungC2010G2 <zhaiduong26z@gmail.com>
2021-02-24 04:10:04 +0700

    sửa lại class account

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -3,8 +3,25 @@ package model;
 public class Account {
     private int id;
     private String username;
+    private String Status;
     private String password;
     private int role;
+
+    public Account(int id, String username, String status, String password, int role) {
+        this.id = id;
+        this.username = username;
+        Status = status;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
 
     public Account(int id, String username, String password, int role) {
         this.id = id;
